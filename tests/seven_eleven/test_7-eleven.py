a7e7521ddb8f2b9036e22e131c0d48160ed37f17
Thitiyaproud <Thitiyaproud@gmail.com>
2024-01-03 08:43:56 +0700

    passed

---
+++ tests/seven_eleven/test_7-eleven.py
@@ -0,0 +1,13 @@
+"""Test cases for 7-eleven"""
+
+#Standard library
+
+#3rd Party library
+import pytest
+
+#project library
+#from seven_eleven.seven from print_7_eleven
+
+#---------------------------------------------------------------------------
+def test_print_7_eleven():
+    """Print 7-eleven."""
